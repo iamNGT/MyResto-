6ba79a0aa60f18052dfa42f005ba3185faaeed1d
iamNGT <guytresor1811@gmail.com>
2020-12-03 22:17:25 +0200

    second commit: small changes

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ const selectElement = (element) => document.querySelector(element);
 selectElement('.menu-icons').addEventListener('click',function () {
     selectElement('nav').classList.toggle('active');
 });
+
+//on scroll of window effects
+
+window.addEventListener('scroll', function(){
+    if(this.scrollY > 20){
+        selectElement('nav').classList.toggle('onscroll');
+    } else{
+        selectElement('nav').classList.remove('onscroll');
+    }
+});
